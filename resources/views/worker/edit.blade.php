b304adbb4db168b43fe1d6060344ba565ee6c039
StevenU21 <ulloadeifheltsteven@gmail.com>
2022-11-08 01:44:50 -0600

    refactor: modificando codigo de rutas

--- resources/views/worker/edit.blade.php
+++ resources/views/worker/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.panel')
 
 @section('template_title')
     Update Worker
@@ -20,7 +20,45 @@
                             {{ method_field('PATCH') }}
                             @csrf
 
-                            @include('worker.form')
+                            <div class="box box-info padding-1">
+                                <div class="box-body">
+
+                                    <div class="form-group">
+                                        {{ Form::label('Nombres') }}
+                                        {{ Form::text('name', $worker->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Nombres']) }}
+                                        {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
+                                    </div>
+                                    <div class="form-group">
+                                        {{ Form::label('Apellidos') }}
+                                        {{ Form::text('last_name', $worker->last_name, ['class' => 'form-control' . ($errors->has('last_name') ? ' is-invalid' : ''), 'placeholder' => 'Apellidos']) }}
+                                        {!! $errors->first('last_name', '<div class="invalid-feedback">:message</div>') !!}
+                                    </div>
+                                    <div class="form-group">
+                                        {{ Form::label('Telefono Personal') }}
+                                        {{ Form::text('phone_number', $worker->phone_number, ['class' => 'form-control' . ($errors->has('phone_number') ? ' is-invalid' : ''), 'placeholder' => 'Telefono Personal']) }}
+                                        {!! $errors->first('phone_number', '<div class="invalid-feedback">:message</div>') !!}
+                                    </div>
+                                    <div class="form-group">
+                                        {{ Form::label('Direccion') }}
+                                        {{ Form::text('address', $worker->address, ['class' => 'form-control' . ($errors->has('address') ? ' is-invalid' : ''), 'placeholder' => 'Direccion']) }}
+                                        {!! $errors->first('address', '<div class="invalid-feedback">:message</div>') !!}
+                                    </div>
+                                    <div class="form-group">
+                                        {{ Form::label('Especialidad') }}
+                                        {{ Form::text('specialty', $worker->specialty, ['class' => 'form-control' . ($errors->has('specialty') ? ' is-invalid' : ''), 'placeholder' => 'Especialidad']) }}
+                                        {!! $errors->first('specialty', '<div class="invalid-feedback">:message</div>') !!}
+                                    </div>
+                                    <div class="form-group">
+                                        {{ Form::label('ID Categoria') }}
+                                        {{ Form::text('category_id', $worker->category_id, ['class' => 'form-control' . ($errors->has('category_id') ? ' is-invalid' : ''), 'placeholder' => 'ID Categoria']) }}
+                                        {!! $errors->first('category_id', '<div class="invalid-feedback">:message</div>') !!}
+                                    </div>
+
+                                </div>
+                                <div class="box-footer mt20">
+                                    <button type="submit" class="btn btn-primary">Enviar</button>
+                                </div>
+                            </div>
 
                         </form>
                     </div>
