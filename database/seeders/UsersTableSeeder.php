c5600c35ed3b9242fe7e11805d594fa4eaa81921
StevenU21 <ulloadeifheltsteven@gmail.com>
2022-11-08 23:00:32 -0600

    Feat: Generando datos falsos

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Steven Ulloa',
+            'email' => 'stevenullos6@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('stevenullos6@gmail.com'), // password
+            'address' => 'del panteoncito 3c sur, 3c oeste',
+            'identity_card' => '161-210501-100K',
+            'role' => 'admin',
+        ]);
+        User::factory()
+            ->count(200)
+            ->create();
+    }
+}
