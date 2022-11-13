c8da3bf7c543321dd52610b77ce202350862b7b5
StevenU21 <ulloadeifheltsteven@gmail.com>
2022-11-12 19:53:36 -0600

    Feat: Integracion Completa del headerDesktop

--- resources/views/includes/panel/headerDesktop.blade.php
+++ resources/views/includes/panel/headerDesktop.blade.php
@@ -1,16 +1,14 @@
-<header>
+<header class="header-v4">
     <!-- Header desktop -->
     <div class="container-menu-desktop">
         <!-- Topbar -->
         <div class="top-bar">
-
             <div class="content-topbar flex-sb-m h-full container">
                 <div class="left-top-bar">
                     Sistema Inteligente de Servicios Profesional
                 </div>
 
                 <div class="right-top-bar flex-w h-full">
-
                     @if (Route::has('login') && Auth::check() && auth()->user()->role == 'admin')
                         <a href="{{ url('/home') }}" class="flex-c-m trans-04 p-lr-25">Panel Administrador</a>
                     @elseif (Route::has('login') && Auth::check() && auth()->user()->role == 'cliente')
@@ -24,27 +22,27 @@
                      @elseif (Route::has('login') && !Auth::check())
                         <a href="{{ url('/login') }}" class="flex-c-m trans-04 p-lr-25">Iniciar Sesion</a>
                     @endif
-
                 </div>
+
             </div>
         </div>
 
-        <div class="wrap-menu-desktop">
+        <div class="wrap-menu-desktop how-shadow1">
             <nav class="limiter-menu-desktop container">
 
                 <!-- Logo desktop -->
                 <a href="#" class="logo">
-                    <img src="images/logo-simp.png" alt="IMG-LOGO">
+                    <img src="images/icons/logo-simp.png" alt="IMG-LOGO">
                 </a>
 
                 <!-- Menu desktop -->
                 <div class="menu-desktop">
                     <ul class="main-menu">
-                        <li class="active-menu">
+                        <li>
                             <a href="/">Inicio</a>
                         </li>
 
-                        <li>
+                        <li class="active-menu">
                             <a href="/producto">Tienda</a>
                         </li>
 
@@ -76,13 +74,12 @@
                         <i class="zmdi zmdi-shopping-cart"></i>
                     </div>
 
-                    <a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="0">
+                    <a href="#" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="0">
                         <i class="zmdi zmdi-favorite-outline"></i>
                     </a>
                 </div>
             </nav>
         </div>
-
     </div>
 
     <!-- Header Mobile -->
@@ -127,52 +124,55 @@
 
             <li>
                 <div class="right-top-bar flex-w h-full">
-                    <a href="#" class="flex-c-m p-lr-10 trans-04">
-                        Ayuda
-                    </a>
+                    @if (Route::has('login') && Auth::check() && auth()->user()->role == 'admin')
+                        <a href="{{ url('/home') }}" class="flex-c-m trans-04 p-lr-25">Panel Administrador</a>
+                    @elseif (Route::has('login') && Auth::check() && auth()->user()->role == 'cliente')
+                    <a href="{{ url('/') }}" class="flex-c-m trans-04 p-lr-25">Mi Cuenta de Usuario</a>
+                    <a href="{{ url('logout') }}" class="flex-c-m trans-04 p-lr-25"
+                    onclick="event.preventDefault(); document.getElementById('formLogout').submit();">Cerrar Sesion</a>
+                    <form action="{{ route('logout') }}" method="POST" style="display: none;" id="formLogout">
+                        @csrf
+                      </form>
 
-                    <a href="dashboard.html" class="flex-c-m p-lr-10 trans-04">
-                        Mi Cuenta
-                    </a>
-
-                    <a href="#" class="flex-c-m p-lr-10 trans-04">
-                        ENG
-                    </a>
-
-                    <a href="#" class="flex-c-m p-lr-10 trans-04">
-                        ESP
-                    </a>
+                     @elseif (Route::has('login') && !Auth::check())
+                        <a href="{{ url('/login') }}" class="flex-c-m trans-04 p-lr-25">Iniciar Sesion</a>
+                    @endif
                 </div>
             </li>
+
         </ul>
 
         <ul class="main-menu-m">
             <li>
-                <a href="/">Inicio</a>
-
+                <a href="index.html">Home</a>
+                <ul class="sub-menu-m">
+                    <li><a href="index.html">Homepage 1</a></li>
+                    <li><a href="home-02.html">Homepage 2</a></li>
+                    <li><a href="home-03.html">Homepage 3</a></li>
+                </ul>
                 <span class="arrow-main-menu-m">
                     <i class="fa fa-angle-right" aria-hidden="true"></i>
                 </span>
             </li>
 
             <li>
-                <a href="/producto"> Tienda</a>
+                <a href="product.html">Shop</a>
             </li>
 
             <li>
-                <a href="shoping-cart.html" class="label1 rs1" data-label1="hot">Caracteristicas</a>
+                <a href="shoping-cart.html" class="label1 rs1" data-label1="hot">Features</a>
             </li>
 
             <li>
-                <a href="blog.html">Comunidad</a>
+                <a href="blog.html">Blog</a>
             </li>
 
             <li>
-                <a href="about.html">Acerca de</a>
+                <a href="about.html">About</a>
             </li>
 
             <li>
-                <a href="/contac">Contacto</a>
+                <a href="contact.html">Contact</a>
             </li>
         </ul>
     </div>
